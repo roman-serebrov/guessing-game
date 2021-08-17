3182cfeea012eaec56939bac517966ff4bbad927
Roman Serebrov <romanspb00@mail.ru>
2021-08-17 23:19:14 +0300

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0
+        this.min = 0
+        this.num = null
+    }
 
     setRange(min, max) {
+        this.max = max
+        this.min = min
 
     }
 
     guess() {
-
+        this.num = (this.min + this.max) / 2
+        return Math.ceil(this.num)
+        
     }
 
     lower() {
-
+        return this.max =  Math.ceil(this.num)
     }
 
     greater() {
-
+        return this.min = Math.ceil(this.num)
     }
 }
 
